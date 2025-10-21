e350a5fcf353205a694a58ad4ed21d4ba4bff9d2
Cowboy2009 <mateusz.morzy@infotech.edu.pl>
2025-10-21 14:38:59 +0200

    test2

--- gracz.gd
+++ gracz.gd
@@ -7,7 +7,7 @@ var screen_size = Vector2(1152, 600)
 #func _ready() -> void:
 	#hide()
 
-#sssss
+#ssssssssss
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
